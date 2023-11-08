532d8da5ee91869eebc08e912cca4a7bc6836f2c
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2023-11-07 23:58:52 -0500

    Add files via upload

---
+++ magicEightBall.js
@@ -0,0 +1,11 @@
+// create a variable 'userName, and set variable to the user's name
+let userName = 'Ross';
+// create a ternary expression that decides what to do if the user enters a name or not
+userName === 'Jane' ? console.log('Hello, Jane!') : console.log('Hello!');
+// create a variable 'userQuestion' and add a string value asking a question
+const userQuestion = 'How old are you?';
+// log the variable userQuestion
+console.log(userQuestion);
+// generate a random number between 0 and 7
+let randomNumber = Math.floor(Math.random() * 8);
+// create another variable eigthBall, set to empty string. 
