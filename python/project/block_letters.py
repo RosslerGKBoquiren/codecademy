57b1ab1bd8de56dbffba9ebbd66429d9c785fe6c
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-01-29 20:29:51 -0500

    Add files via upload

---
+++ python/project/block_letters.py
@@ -0,0 +1,14 @@
+# My initials are RB
+#First name: Ross
+#last name: Boquiren
+#fun fact: I'm a jack of all trades
+
+# Output your first initial as a block letter.
+print('RRRR   BBBB')
+# Output your second initial as a block letter by adding to the print() statements.
+print('R   R  B   B')
+print('R   R  B   B')
+print('RRRR   BBBB')
+print('R R    B   B')
+print('R  R   B   B')
+print('R   R  BBBB')
