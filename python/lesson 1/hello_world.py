d43d17f6c555ad497ae3bb7d817b231d9a44dfb2
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-01-29 20:11:03 -0500

    Add files via upload

---
+++ python/lesson 1/hello_world.py
@@ -0,0 +1,11 @@
+my_age = 30;
+half_my_age = 30 / 2;
+greeting = "Hello World!";
+name = "Ross!";
+greeting_with_name = greeting + ' My name is ' + name;
+
+print(my_age);
+print(half_my_age);
+print(greeting);
+print(name);
+print(greeting_with_name);
