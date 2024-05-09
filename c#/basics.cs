3076c4362e3efe926d8e5bc22f229e81ea1513ec
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-05-09 00:32:21 -0400

    Add files via upload

--- c#/basics.cs
+++ c#/basics.cs
@@ -1,13 +1,23 @@
-// Change the code in the editor so that your own name is printed to the console!
+// Getting input
+we can also read input from a user. The command Console.ReadLine() captures text
+that a user types into the console. 
+
 using System;
 
-namespace HelloWorld
+namespace GettingInput
 {
   class Program
   {
     static void Main()
     {
-      Console.WriteLine("Ross");    
-     }
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();  // Console.ReadLine captures user input in the console
+      Console.WriteLine($"You are {input} years old!"); // Console.WriteLine prints text to the console
+    }
   }
 }
+
+
+// Comments
+//
+/* */
---
+++ c#/console_creatures.cs
@@ -0,0 +1,58 @@
+using System;
+namespace ConsoleCreatures
+{
+	class Program
+	{
+		static void Main()
+		{	
+			// the purpose of this program is to draw a console creature
+			Console.WriteLine(" .-.");
+			 // draw the eyes
+	      	Console.WriteLine("(o o)");  
+	      	// draw the mouth
+	      	Console.WriteLine("| 0 |");
+	      	// draw the body
+	        Console.WriteLine("|   |");
+	        Console.WriteLine("'~~~'");
+	        // name the ghost
+	        Console.WriteLine("The Ghost");
+
+
+
+	        // draw the dancing ghost
+	        Console.WriteLine("c_     ___");
+	        Console.WriteLine("  \\\\__|__|__");
+	        Console.WriteLine("　　 \\( o_o)");
+	        Console.WriteLine("　　　 > ~  >");
+	        Console.WriteLine("　　　/ 　 / \\\\");
+	        Console.WriteLine("　　 /　　/　 \\\\");
+	        Console.WriteLine("　　 \\　 )　　c/");
+	        Console.WriteLine("　　/　 /");
+	        Console.Writeline("　 /　/|");
+	        Console.Writeline("　(　( \\ ");
+	        Console.Writeline("　|　|  \\ ");
+	        Console.Writeline("　| / \\ )");
+	        Console.Writeline("　| |　) |");
+	        Console.Writeline(" /  )(_/");
+	        Console.Writeline("(_ /");
+	        Console.Writeline("The Dancing Ghost");
+
+
+
+	        //Draw the letters RO
+	        Console.Writeline("RRRRR         OOOO");
+	        Console.Writeline("RR  RR       OO  OO");
+	        Console.Writeline("RR  RRR     OO    OO");
+	        Console.Writeline("RR  RRR     OO    OO");
+	        Console.Writeline("RR  RR      OO    OO");
+	        Console.Writeline("RRRRR       OO    OO");
+	        Console.Writeline("RRRRR       OO    OO");
+	        Console.Writeline("RR  RR      OO    OO");
+	        Console.Writeline("RR   RR     OO    OO");
+	        Console.Writeline("RR    RR     OO  OO");
+	        Console.Writeline("RR     RR     OOOO");
+	        Conwole.Writeline("The RO");
+		}
+	}
+}
+
