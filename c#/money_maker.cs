877bf273ef7fd9e3236abdbc62fa19963c4fe626
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-07-31 12:37:37 -0400

    Update money_maker.cs

--- c#/money_maker.cs
+++ c#/money_maker.cs
@@ -7,36 +7,21 @@ namespace MoneyMaker
     public static void Main(string[] args)
     {
       Console.WriteLine("Welcome to Money Maker!");
-      // ask the user for the amount to convert
-      Console.Write("Enter the amount to convert: ");
-      // capture the amount in a variable
-      string amtConvert = Console.ReadLine();
-      // convert the captured value to a number
-      double amtConverted = Convert.ToDouble(amtConvert);
-      // let the user know what you are about to do
-      Console.WriteLine($"{amtConverted} cents is equal to...");
+      Console.Write("\nEnter an amount to convert: ");
+      double amountToConvert = Convert.ToDouble(Console.ReadLine());
 
-      // define two variables that holds the values of gold and silver
-      int goldCoin = 10;
-      int silverCoin = 5;
+      Console.WriteLine($"\n{amountToConvert} is equal to...");
 
-      /* find the maximum number of gold coin 
-      divide the amount by the value of a gold coin.
-      round down to nearest integer
-      then store the result in a double variable called goldCoins*/
-      double goldCoins = Math.Floor(amtConverted / goldCoin);
-      // use modulo to find the remaining amount and store it in a double variable
-      double leftover = amtConverted % goldCoin;
-      // find the maximum number of silver coins that fit into the remainder
-      double silverCoins = Math.Floor(leftover / silverCoin);
-      // use modulo to find the remaining amount and store it in the existing remainder value
-      leftover = leftover % silverCoin;
+      int gold = 10;
+      int silver = 5;
 
-      // print out all of the coins
-      Console.WriteLine($"Gold coins: {goldCoins}");
-      Console.WriteLine($"Silver coins: {silverCoins}");
-      Console.WriteLine($"Bronze coins: {leftover}");
+      double goldCoins = Math.Floor(amountToConvert / gold);
+      double remainder = amountToConvert % gold;
 
+      double silverCoins = Math.Floor(remainder / silver);
+      remainder = remainder % silver;
+
+      Console.WriteLine($"Gold coins: {goldCoins} \nSilver coins: {silverCoins} \nBronze coins: {remainder}");
     }
   }
 }
